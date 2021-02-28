6e6431822ce34a9fdf6ccf9f5f6d134609a8bb2c
linwie <286645611@qq.com>
2021-02-28 15:41:13 +0800

    test

--- src/layout/components/Sidebar/SidebarItem.vue
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -1,10 +1,47 @@
 <template>
-  <div></div>
+  <div>
+    <template v-for="item of per_routes">
+      <template v-if="item.children && item.children.length === 1">
+        <a-menu-item
+          :key="item.path"
+          style="text-align: left"
+          @click="navPage(item)"
+          v-if="!item.hidden"
+        >
+          <a-icon
+            :type="item.children[0].meta.icon ? item.children[0].meta.icon : ''"
+          />
+          <span class="nav-text">{{ item.children[0].meta.title }}</span>
+        </a-menu-item>
+      </template>
+      <template v-else>
+        <a-sub-menu
+          :key="item.path"
+          style="text-align: left"
+          v-if="!item.hidden"
+        >
+          <span slot="title">
+            <a-icon :type="item.meta.icon ? item.meta.icon : ''" />
+            <span>{{ item.meta.title }}</span>
+          </span>
+          <template v-for="child of item.children">
+            <a-menu-item
+              :key="child.path"
+              style="text-align: left"
+              @click="navPage(child)"
+            >
+              {{ child.meta.title }}
+            </a-menu-item>
+          </template>
+        </a-sub-menu>
+      </template>
+    </template>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "",
+  name: "SidebarItem",
   data() {
     return {};
   },
