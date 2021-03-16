fa3419813346de9e281eaf431a58a52be2999dd4
linwie <286645611@qq.com>
2021-03-16 17:59:53 +0800

    fix: 修改mock

--- vue.config.js
+++ vue.config.js
@@ -7,27 +7,7 @@
 
 // const name = defaultSettings.title || 'vue Element Admin' // page title
 
-// // If your port is set to 80,
-// // use administrator privileges to execute the command line.
-// // For example, Mac: sudo npm run
-// // You can change the port by the following method:
-// // port = 9527 npm run dev OR npm run dev --port = 9527
-// const port = process.env.port || process.env.npm_config_port || 9527 // dev port
 module.exports = {
-  // publicPath: '/',
-  // outputDir: 'dist',
-  // assetsDir: 'static',
-  // lintOnSave: process.env.NODE_ENV === 'development',
-  // productionSourceMap: false,
-  // devServer: {
-  //   port: port,
-  //   open: true,
-  //   overlay: {
-  //     warnings: false,
-  //     errors: true
-  //   },
-  //   before: require('./mock/mock-server.js')
-  // },
   css: {
     loaderOptions: {
       // 向 CSS 相关的 loader 传递选项
